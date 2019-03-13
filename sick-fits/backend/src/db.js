93e78bc453cdbc3aec5404104c09d1465ff42239
Horacio Gutierrez <hogutier@yahoo.com>
2019-03-13 15:30:12 -0600

    Complete module 12

---
+++ sick-fits/backend/src/db.js
@@ -0,0 +1,12 @@
+//THIS FILE CONNECTS TO THE REMOTE PRISMA DB AND GIVES US THE ABILITY TO QUERY IT WITH JAVASCRIPT
+
+const { Prisma } = require('prisma-binding');
+
+const db = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: false
+});
+
+module.exports = db;
